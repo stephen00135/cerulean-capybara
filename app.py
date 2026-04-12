ae9f0f4b2b2c3e4c605c23479fad07fc475e1491
Stephen Heflin <sh00135@mix.wvu.edu>
2026-04-11 20:33:05 -0400

    Simple example

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return "Hello World"
+    name = None
+
+    if request.method == 'POST':
+        name = request.form.get('name', '').strip()
+    return render_template('home', name=name)
