424ec1d1bf6cdbb31a509850acec1659fa399d3f
Stephen Heflin <sh00135@mix.wvu.edu>
2026-04-15 21:00:55 -0400

    Started employee page

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,41 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import (StringField, PasswordField, BooleanField,
+                      SubmitField, SelectField, IntegerField, FormField)
+from wtforms.validators import DataRequired, Optional, Length, NumberRange
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class TransactionForm(FlaskForm):
+    transaction_type = SelectField(
+        'Type',
+        choices=[
+            ('sale', 'sale'),
+            ('trade', 'trade'),
+            ('return', 'return'),
+        ],
+        validators=[DataRequired()]
+    )
+    customer = StringField('Customer Email', validators=[Optional(), Length(max=100)])
+    employee = StringField('Employee Email', validators=[Optional(), Length(max=100)])
+    payment_method = SelectField(
+        'Type',
+        choices=[
+            ('credit', 'credit'),
+            ('debit', 'debit'),
+            ('cash', 'cash')
+        ],
+        validators=[DataRequired()]
+    )
+    submit = SubmitField('Submit')
+
+class ProductForm(FlaskForm):
+    product_id = IntegerField('Product ID', validators=[DataRequired()])
+
+class TransactionItemForm(FlaskForm):
+    product = FormField(ProductForm)
+    quantity = IntegerField('Quantity', validators=[DataRequired(), NumberRange(min=1, max=50)])
+
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,26 @@
 from flask import render_template, flash, redirect
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, TransactionForm, TransactionItemForm
 
 @app.route('/')
 @app.route('/index')
-@app.route('/employee')
+@app.route('/employee', methods=['GET', 'POST'])
 def employee():
-    return render_template('employee.html', title='Employee')
+    transaction_form = TransactionForm()
+    item_form = TransactionItemForm()
+    if transaction_form.validate_on_submit() and item_form.validate():
+        transaction_type = transaction_form.transaction_type.data
+        customer = transaction_form.customer.data
+        employee = transaction_form.employee.data
+        payment_method = transaction_form.payment_method.data
+        product_id = item_form.product.product_id.data
+        quantity = item_form.quantity.data
+        flash('Transaction submitted')
+        return redirect('/index')
+    return render_template('employee.html', title='Employee',
+                           transaction_form=transaction_form, item_form=item_form)
 
-@app.route('/manager')
+@app.route('/manager', methods=['GET', 'POST'])
 def manager():
     return render_template('manager.html', title='Manager')
 
